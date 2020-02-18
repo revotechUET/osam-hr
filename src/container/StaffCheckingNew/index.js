4d23761595d9f29fae8c1e0b8ae7d1dd04b14a97
ThiinhUET <thinhlevan2015@gmail.com>
2020-02-18 09:40:34 +0700

    update

--- src/container/StaffCheckingNew/index.js
+++ src/container/StaffCheckingNew/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import BorderedContainer from "./../../components/BorderedContainer";
-import BorderBottomInput from "./../../components/BorderBottomInput";
 
 
 import './style.less'
@@ -9,39 +8,79 @@ import './style.less'
 class StaffCheckingNewPage extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
+    this.state = {
+      staffName: '',
+      date: '',
+      checkIn: '',
+      checkOut: '',
+      note: ''
+    }
+
+    this.handleCancle = this.handleCancle.bind(this);
+    this.handleStaffNameChange = this.handleStaffNameChange.bind(this);
+    this.handleCheckInChange = this.handleCheckInChange.bind(this);
+    this.handleCheckOutChange = this.handleCheckOutChange.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleNoteChange = this.handleNoteChange.bind(this);
+    this.handleSave = this.handleSave.bind(this);
   }
 
+  handleSave() {
+    console.log(staffName, date, checkIn, checkOut, note);
+  }
+  handleCheckInChange(evt) {
+    this.setState({ checkIn: evt.target.value });
+  }
+
+  handleCheckOutChange(evt) {
+    this.setState({ checkOut: evt.target.value });
+  }
+
+  handleCancle() {
+    this.props.history.push('/checking');
+  }
+
+  handleStaffNameChange(evt) {
+    this.setState({ staffName: evt.target.value });
+  }
+
+  handleDateChange(evt) {
+    this.setState({ date: evt.target.value });
+  }
+
+  handleNoteChange(evt) {
+    this.setState({ note: evt.target.value });
+  }
   render() {
     return (
-      <div className = "StaffCheckingNew">
+      <div className="StaffCheckingNew">
         <h1 style={{ marginBottom: "10px" }}>Chấm công / Mới</h1>
         <div style={{ display: "flex" }}>
-          <div className="my-button active-btn">Lưu</div>
-          <div className="my-button">Hủy</div>
+          <button className="my-button active-btn" onClick={this.handleSave}>Lưu</button>
+          <button className="my-button" onClick={this.handleCancle}>Hủy</button>
         </div>
         <BorderedContainer>
           <h3>Mới</h3>
           <div className="input-field">
-            <div className = "label">Nhân Viên</div>
-            <input className = "input"/>
+            <div className="label">Nhân Viên</div>
+            <input className="input" value={this.state.staffName} onChange={this.handleStaffNameChange} />
           </div>
           <div className="input-field">
-            <div className = "label">Ngày</div>
-            <input type="date"/>
+            <div className="label">Ngày</div>
+            <input type="date" value={this.state.date} onChange={this.handleCheckInChange}/>
           </div>
-        
-          <div className = "input-field">
-            <div className = "label">Check in</div>
-            <input type="time" />
-          </div>
-          <div className = "input-field">
-            <div className = "label">Check out</div>
-            <input type="time" />
+
+          <div className="input-field">
+            <div className="label">Check in</div>
+            <input type="time" value={this.state.checkIn} onChange={this.handleCheckInChange} />
           </div>
           <div className="input-field">
-            <div className = "label">Ghi chú</div>
-            <input className = "input"/>
+            <div className="label">Check out</div>
+            <input type="time" value={this.state.checkOut} onChange={this.handleCheckOutChange} />
+          </div>
+          <div className="input-field">
+            <div className="label">Ghi chú</div>
+            <input className="input" value={this.state.note} onChange={this.handleNoteChange} />
           </div>
         </BorderedContainer>
       </div>
