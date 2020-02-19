5fdba037dc2621c206a67cdc4c54e06a954972d8
ThiinhUET <thinhlevan2015@gmail.com>
2020-02-19 22:42:32 +0700

    update

--- gas/src/routes/department.ts
+++ gas/src/routes/department.ts
@@ -1,10 +1,12 @@
 import {Department} from '../@types/department';
 import {db} from '../db';
+import {uuid} from '../utils';
 
 global.addNewDepartment = addNewDepartment;
 global.listDepartment   = listDepartment;
 
-function addNewDepartment(data){
+function addNewDepartment(data : Department){
+    data.id = uuid();
     return db.from<Department>('department').insert(data);
 }
 
--- src/container/DepartmentNew/index.js
+++ src/container/DepartmentNew/index.js
@@ -3,69 +3,74 @@ import { withRouter } from "react-router-dom";
 import BorderedContainer from "./../../components/BorderedContainer";
 import BorderBottomInput from "./../../components/BorderBottomInput";
 import ChipsContainer from './../../components/ChipsContainer';
-import './style.less';
+import Autocomplete from "../../components/Autocomplete";
 import apiService from '../../service/api.service';
 
+import './style.less';
 class DepartmentNewPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      departmentName : '',
-      manager        : '',
-      active         : true,
-      countActive    : 0,
-      name           : ''
+      departmentName: '',
+      manager: null,
+      idManager: null,
+      active: true,
+      countActive: 0,
+      name: '',
+      approvers: null,
+      idApprovers: null,
+      loading: false,
+
     };
-    this.handleManagerChange    = this.handleManagerChange.bind(this);
-    this.handleSave             = this.handleSave.bind(this);
-    this.handleCancel           = this.handleCancel.bind(this);
+    this.handleManagerChange = this.handleManagerChange.bind(this);
+    this.handleSave = this.handleSave.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
     this.handleDepartmentChange = this.handleDepartmentChange.bind(this);
-    this.handleActiveStatus     = this.handleActiveStatus.bind(this);
+    this.handleActiveStatus = this.handleActiveStatus.bind(this);
   }
 
-  async handleSave(event){
-    let id = apiService.generateUid();
+  async handleSave() {
     let data = {
-      "id"          : id,
-      "name"        : this.state.departmentName,
-      "active"      : this.state.active.toString(),
-      "idManager"   : "Le Van Thinh",
-      "idApprovers" : "Mr.Thinh"
-    }
-    let newSheetData = await apiService.addNewDepartment(data);
-    console.log(newSheetData);
-    event.preventDefault();
+      name: this.state.departmentName,
+      active: this.state.active.toString(),
+      idManager: this.state.idRequester,
+      idApprovers: this.state.idApprovers,
+      active     : this.state.active   
+    };
+    let addNewDepartment = await apiService.addNewDepartment(data);
+    console.log(addNewDepartment);
   }
 
-  handleCancel(){
+  handleCancel() {
     this.props.history.push('/departments');
   }
 
-  handleManagerChange(event){
-    this.setState({manager : event.target.value});
+  handleManagerChange(event) {
+    this.setState({ manager: event.target.value });
   }
 
-  handleDepartmentChange(event){
-    this.setState({departmentName : event.target.value});
+  handleDepartmentChange(event) {
+    this.setState({ departmentName: event.target.value });
   }
 
-  async componentDidMount(){
-    
-  }
 
-  handleActiveStatus(){
+  handleActiveStatus() {
     this.state.countActive += 1;
-    if(this.state.countActive % 2 === 0){
-      this.setState({active : false});
+    if (this.state.countActive % 2 === 0) {
+      this.setState({ active: false });
     }
-    else if (this.state.countActive % 2 !== 0){
-      this.setState({active : true});
+    else if (this.state.countActive % 2 !== 0) {
+      this.setState({ active: true });
     }
   }
 
+  async componentDidMount() {
+    let users = await apiService.listUsers();
+    this.setState({ manager: users, approvers: users });
+  }
   render() {
     return (
-      <div className = "DepartmentNew">
+      <div className="DepartmentNew">
         <h1 style={{ marginBottom: "10px" }}>Bộ Phận / Mới</h1>
         <div style={{ display: "flex" }}>
           <button className="my-button active-btn" onClick={this.handleSave}>Lưu</button>
@@ -73,20 +78,40 @@ class DepartmentNewPage extends React.Component {
         </div>
         <BorderedContainer>
           <h3>Tên Bộ Phận</h3>
-          <BorderBottomInput placeholder="Tên Bộ Phận" value = {this.state.departmentName} onChange={this.handleDepartmentChange}/>
+          <BorderBottomInput placeholder="Tên Bộ Phận" value={this.state.departmentName} onChange={this.handleDepartmentChange} />
           <div className="input-field">
-            <div className = "label">Người quản lý</div>
-            <input className = "input" value={this.state.manager} onChange={this.handleManagerChange}/>
+            <div className="label">Người quản lý</div>
+            <Autocomplete
+              loading={this.state.manager === null}
+              style={{ flex: 1 }}
+              options={this.state.manager}
+              keyProp='id'
+              labelProp='name'
+              onChange={(event, value) => {
+                this.setState({ idRequester: value && value.id });
+                console.log(this.state.idRequester);
+              }}
+            />
           </div>
           <div className="input-field">
-            <div className = "label">Người phụ trách duyệt leave request</div>
-            <div className = "input">
-              <ChipsContainer/>
-            </div>
+            <div className="label">Người phụ trách duyệt leave request</div>
+            <Autocomplete
+              multiple
+              filterSelectedOptions
+              loading={this.state.approvers === null}
+              style={{ flex: 1 }}
+              options={this.state.approvers}
+              keyProp='id'
+              labelProp='name'
+              onChange={(event, value) => {
+                this.setState({ idApprovers: value && value.id });
+                console.log(this.state.idApprovers);
+              }}
+            />
           </div>
           <div className="input-field">
-            <div className = "label">Hoạt động</div>
-            <input className = "input checkbox" type="checkbox" onClick={this.handleActiveStatus}/>
+            <div className="label">Hoạt động</div>
+            <input className="input checkbox" type="checkbox" checked={this.state.active} onClick={this.handleActiveStatus} />
           </div>
         </BorderedContainer>
       </div>
