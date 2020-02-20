6bfac2c9d4f9660b245e287d80eba20666f2fe14
hungnk96 <hungnk1996@gmail.com>
2020-02-20 21:14:37 +0700

    update

--- src/container/ChekingDetail.js
+++ src/container/ChekingDetail.js
@@ -35,24 +35,44 @@ class StaffDetailPage extends React.Component {
         return (
             <div className="StaffDetail">
                 <div className="title-vs-btn">
+                    <div className="my-button active-btn ti ti-pencil"></div>
                     <div className="my-button active-btn ti ti-check" style={{ background: "linear-gradient(120deg, #67dc2c, #38c53e)" }}></div>
                     <div className="my-button ti ti-close" style={{ background: "#ddd", boxShadow: "none", color: "#888" }}></div>
                     <div className="title">Chấm công / <span className="uppercase">{dateFormat(this.state.checkDetail.date, 'dd/MM/yyyy')}</span></div>
                 </div>
 
                 <BorderedContainer>
-                    <span>Nhân viên</span>
-                    <div>{this.state.checkDetail.requester.name}</div>
-                    <span>Ngày</span>
-                    <div>{dateFormat(this.state.checkDetail.date, 'dd/MM/yyyy')}</div>
-                    <span>Check in</span>
-                    <div>{dateFormat(this.state.checkDetail.checkinTime, 'hh:mm')}</div>
-                    <span>Check out</span>
-                    <div>{dateFormat(this.state.checkDetail.checkoutTime, 'hh:mm')}</div>
-                    <span>Ghi chú</span>
-                    <div>{this.state.checkDetail.note}</div>
-                    <span>Báo cáo</span>
-                    <div>{this.state.checkDetail.reportContent}</div>
+                    <div className="item-detail">
+                    {/* <div className="image-reject"></div> */}
+                        {/* <div className="image-accept"></div> */}
+                        <div className="infor-item-detail">
+                            <span>Nhân viên</span>
+                            <div style={{fontWeight: "bold", fontSize: "150%", marginBottom: "20px"}}>{this.state.checkDetail.requester.name}</div>
+                            <div style={{display: "flex", flexDirection: "column"}}>
+                            <div style={{display: "flex", marginBottom: "10px"}}>
+                                <div style={{flexBasis: "120px", fontWeight: "bold"}}>Ngày</div>
+                                <div style={{flex: 1}}>{dateFormat(this.state.checkDetail.date, 'dd/MM/yyyy')}</div>
+                            </div>
+                            <div style={{display: "flex", marginBottom: "10px"}}>
+                                <div style={{flexBasis: "120px", fontWeight: "bold"}}>Check in</div>
+                                <div style={{flex: 1}}>{dateFormat(this.state.checkDetail.checkinTime, 'hh:mm')}</div>
+                            </div>
+                            <div style={{display: "flex", marginBottom: "10px"}}>
+                                <div style={{flexBasis: "120px", fontWeight: "bold"}}>Check out</div>
+                                <div style={{flex: 1}}>{dateFormat(this.state.checkDetail.checkoutTime, 'hh:mm')}</div>
+                            </div>
+                            <div style={{display: "flex", marginBottom: "10px"}}>
+                                <div style={{flexBasis: "120px", fontWeight: "bold"}}>Ghi chú</div>
+                                <div style={{flex: 1}}>{this.state.checkDetail.note}</div>
+                            </div>
+                            <div style={{display: "flex", marginBottom: "10px"}}>
+                                <div style={{flexBasis: "120px", fontWeight: "bold"}}>Báo cáo</div>
+                                <div style={{flex: 1}}>{this.state.checkDetail.reportContent}</div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                   
                     
                 </BorderedContainer>
             </div>
