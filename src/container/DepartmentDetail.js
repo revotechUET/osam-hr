bc443d24b5272edfd219d83243ce1240cf350715
ThiinhUET <thinhlevan2015@gmail.com>
2020-02-22 17:24:11 +0700

    aaa

--- src/container/DepartmentDetail.js
+++ src/container/DepartmentDetail.js
@@ -13,7 +13,7 @@ class DepartmentDetailPage extends React.Component {
             loading: true,
             departmentDetail: null,
             manager : null,
-            approvers : null
+            approvers : []
         }
         this.edit = this.edit.bind(this);
         this.response = this.response.bind(this);
@@ -29,8 +29,13 @@ class DepartmentDetailPage extends React.Component {
             }
         });
         let manager = users.filter((u) => u.id === this.state.departmentDetail.idManager);
-        // let approvers = users.filter((u) => this.state.departmentDetail.idApproversu.id);
-        console.log( this.state.departmentDetail.idApprovers.include("113258463329157762545"));
+        let idApprovers = this.state.departmentDetail.idApprovers
+        users.forEach((value, index) => {
+            if(idApprovers.includes(value.id)){
+                this.state.approvers.push(value.name)
+            }
+        })
+        console.log(this.state.approvers);
         this.setState({ loading: false, manager : manager});
     }
 
@@ -52,32 +57,38 @@ class DepartmentDetailPage extends React.Component {
                     <div className="my-button active-btn ti ti-check" style={{ background: "linear-gradient(120deg, #67dc2c, #38c53e)" }} onClick={this.response}></div>
                     <div className="title">Bộ phận / <span>{this.state.departmentDetail.name}</span></div>
                 </div>
-
-            <BorderedContainer>
+                <BorderedContainer>
                 <div className="item-detail">
-                    {/* <div className="image-reject"></div> */}
-                    {/* <div className="image-accept"></div> */}
-                    <div className="infor-item-detail">
-                        <span>Người quản lý</span>
-                        <div style={{ fontWeight: "bold", fontSize: "150%", marginBottom: "20px" }}>{this.state.manager[0].name}</div>
-                        <div style={{ display: "flex", flexDirection: "column" }}>
-                            <div style={{ display: "flex", marginBottom: "10px" }}>
-                                <div style={{ flexBasis: "120px", fontWeight: "bold" }}>Người phụ trách duyệt</div>
-                                {/* <div style={{ flex: 1 }}>{this.state.manager.idApprovers}</div> */}
+                    <div className="image-reject"></div>
+                        <div className="image-accept"></div>
+                        <div className="infor-item-detail">
+                            <span>Người quản lý</span>
+                            <div style={{fontWeight: "bold", fontSize: "150%", marginBottom: "20px"}}>{this.state.manager.name}</div>
+                            <div style={{display: "flex", flexDirection: "column"}}>
+                                <div style={{display: "flex", marginBottom: "10px"}}>
+                                <div style={{flexBasis: "120px", fontWeight: "bold"}}>Ngày</div>
+                                <div style={{flex: 1}}></div>
                             </div>
-                            <div style={{ display: "flex", marginBottom: "10px" }}>
-                                <div style={{ flexBasis: "120px", fontWeight: "bold" }}>Hoạt động</div>
-                                {/* <div style={{ flex: 1 }}>{dateFormat(this.state.checkDetail.checkinTime, 'hh:mm')}</div> */}
+                            <div style={{display: "flex", marginBottom: "10px"}}>
+                                <div style={{flexBasis: "120px", fontWeight: "bold"}}>Check in</div>
+                                <div style={{flex: 1}}></div>
                             </div>
-                            <div style={{ display: "flex", marginBottom: "10px" }}>
-                                <div style={{ flexBasis: "120px", fontWeight: "bold" }}>Danh sách nhân viên</div>
-                                {/* <div style={{ flex: 1 }}>{dateFormat(this.state.checkDetail.checkoutTime, 'hh:mm')}</div> */}
+                            <div style={{display: "flex", marginBottom: "10px"}}>
+                                <div style={{flexBasis: "120px", fontWeight: "bold"}}>Check out</div>
+                                <div style={{flex: 1}}></div>
+                            </div>
+                            <div style={{display: "flex", marginBottom: "10px"}}>
+                                <div style={{flexBasis: "120px", fontWeight: "bold"}}>Ghi chú</div>
+                                <div style={{flex: 1}}></div>
+                            </div>
+                            <div style={{display: "flex", marginBottom: "10px"}}>
+                                <div style={{flexBasis: "120px", fontWeight: "bold"}}>Báo cáo</div>
+                                <div style={{flex: 1}}></div>
+                            </div> */}
                             </div>
                         </div>
                     </div>
-                </div>
-
-            </BorderedContainer>
+                </BorderedContainer>
             </div>
         );
     }
