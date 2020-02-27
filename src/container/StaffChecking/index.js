7d80c5e7597be972f8a0493162f841b5d849fe5e
ThiinhUET <thinhlevan2015@gmail.com>
2020-02-27 15:51:42 +0700

    update

--- src/container/StaffChecking/index.js
+++ src/container/StaffChecking/index.js
@@ -40,29 +40,49 @@ class StaffChecking extends React.Component {
         super(props);
         this.state = {
             staffChecking : [],
-            loading : false
+            loading : false,
+            allChecking  : []
         }
 
+        this.handleFilter = this.handleFilter.bind(this);
+
     }
+
+    handleFilter(event){
+      if(event.target.value === ""){
+        this.setState({staffChecking : this.state.allChecking});
+      }
+      else{
+        let filter =  this.state.staffChecking.filter((e) => {
+          return  e.requester.name.includes(event.target.value);
+        });
+        this.setState({staffChecking : filter});
+      }
+      console.log(event.target.value);
+    }
+    
 
     async componentDidMount(){
         this.setState({loading : true});
         let check = await apiService.listCheck({});
-        this.setState({loading: false , staffChecking : check})
+        this.setState({loading: false , staffChecking : check, allChecking : check});
     }
     render() {
-        let {staffChecking} = this.state;
+        let {staffChecking, filter} = this.state;
         return (<div style={{marginTop: "40px", borderRadius: "10px", padding: "10px 20px", background: "#fff"}}>
             <div className="title-vs-btn">
               <div className="my-button active-btn ti ti-plus" onClick={()=>this.props.history.push("/checking/new")}></div>
               <div className="title">Chấm công</div>
+            </div>
+            <div>
+              <input placeholder="Filter here" onChange = {this.handleFilter} />
             </div>
             <DataTable
             noHeader
             fixedHeader
             fixedHeaderScrollHeight="calc(100vh - 333px)"
             pagination
-            noDataComponent='Loading'
+            noDataComponent='Không có chấm công'
             progressPending={this.state.loading}
             progressComponent={<Loading />}
             persistTableHead
@@ -76,41 +96,6 @@ class StaffChecking extends React.Component {
     }
 }
 
-// function StaffChecking({ history }) {
-//     const [state, setState] = useReducer((prevState, newState) => ({ ...prevState, ...newState }),
-//       {
-//         list: [],
-//         loading: false,
-//       });
-//     const cancellable = apiService.useCancellable();
 
-//     useEffect(() => {
-//       (async () => {
-//         setState({ loading: true })
-//         const list = await cancellable(apiService.listCheck({}));
-//         setState({ list, loading: false });
-//       })();
-//     }, []);
-//     const onRowClicked = useCallback(() => {
-
-//     });
-//     const { list, loading } = state;
-//     return (
-//       <div >
-//         <h1 style={{ marginBottom: "10px" }}>Chấm công</h1>
-//         <button className="my-button active-btn" onClick={() => history.push("/checking/new")}>Tạo mới</button>
-//         <div>
-//           <DataTable
-//             noHeader
-//             noDataComponent='........'
-//             progressPending={loading}
-//             persistTableHead
-//             columns={columns}
-//             data={list}
-//           />
-//         </div>
-//       </div>
-//     )
-//   }
 
 export default withRouter(StaffChecking);
